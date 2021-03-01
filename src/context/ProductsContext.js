2cbaca31eb9b41711f1e1839e9e4bbd4d870a866
daniel <daniel.palmdal@gmail.com>
2021-03-01 22:19:29 +0100

    Data from json imported
    Also added image src to object

--- src/context/ProductsContext.js
+++ src/context/ProductsContext.js
@@ -1,13 +1,29 @@
 import { createContext, useEffect, useState } from "react";
 
+import cars from "../json/cars.json";
+
 export const ProductsContext = createContext();
 
-
 function ProductsContextProvider(props) {
-// State for products after fetching from json
-  //const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(null);
+  useEffect(() => {
+    /* go through every object and add a key value pair for an image src */
+    const carsWithImage = cars.map((car) => {
+      const image = {
+        image: `assets/car-picture/${car.make}-${car.model}-${car.year}`,
+      };
+      // prettier adds the parantheses
+      return (car = { ...car, ...image });
+    });
+    setProducts(carsWithImage);
+  }, []);
+
+
+
+
+  // Insert you methods and values here
   const values = {
-    // Insert you methods and values here
+    products,
   };
 
   return (
