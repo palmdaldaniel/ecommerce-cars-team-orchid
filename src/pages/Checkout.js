31972fb1c44e5d565c06b029b53d4ade9d0e3dba
Amanda <amandaelizaolsson@gmail.com>
2021-03-01 15:50:56 +0100

    Added flex to checkout container

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,8 +2,11 @@ import style from './css/Checkout.module.css'
 
 function Checkout() {
     return(
-        <div className="checkoutContainer">
+        <div className={style.checkoutContainer}>
             <h1>Checkout</h1>
+            {/* Shopping cart list */}
+            {/* Form for personal details */}
+            {/* purchase button */}
         </div>
     )
 }
