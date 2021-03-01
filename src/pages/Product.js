4f4b78ebba81af6586d40f5ed0beae377937b554
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-03-01 15:35:59 +0100

    Create some placeholder content

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,8 +1,15 @@
 function Product(props) {
 
 	return (
-		<div>
-
+		<div className="container">
+			<div className="row">
+				<div className="col">
+					<h1>This is a product</h1>
+				</div>
+				<div className="col">
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, ab, amet totam necessitatibus rerum sapiente itaque neque cum tempore quis veritatis doloribus illo cumque. At culpa natus molestias distinctio commodi?</p>
+				</div>
+			</div>
 		</div>
 	);
 }
