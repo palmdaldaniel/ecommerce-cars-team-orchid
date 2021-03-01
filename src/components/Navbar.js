660400069487a4b792f573b33d51369949966306
Saga <sagaswahn@hotmail.com>
2021-03-01 16:46:01 +0100

    Adding style and fontawesome to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,43 @@
 import { NavLink } from "react-router-dom";
+import React, { useState } from "react"
+
+
 import styles from "./css/Navbar.module.css";
 
 function Navbar () {
+	//use boolean to create toggle effect on hamburger menu
+	const [navbarOpen, setNavbarOpen] = useState(false)
+
+	const handleToggle = () => {
+		setNavbarOpen(prev => !prev)
+	}
+
+	const closeMenu = () => {
+		setNavbarOpen(false)
+	}
+
 	return (
-		<div>
-			<div className={styles.navbarWrapper}>
-					<NavLink to="/">Home</NavLink>
-					<NavLink to="/">About us</NavLink>
-			</div>
-		</div>
+		<nav className="navBar">
+				<div className={styles.navbarWrapper}>
+				<button onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</button>
+					<ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+						<NavLink 
+						to="/"
+						activeClassName="active-link"
+  					onClick={() => closeMenu()}
+  					exact>
+						Home
+						</NavLink>
+						<NavLink 
+						to="/"
+						activeClassName="active-link"
+  					onClick={() => closeMenu()}
+  					exact>
+						About
+						</NavLink>
+					</ul>
+				</div>
+		</nav>
 	)
 }
 
