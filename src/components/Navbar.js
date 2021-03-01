dfc647fcaff8a22773514cb6f8a3800420c4c098
Saga <sagaswahn@hotmail.com>
2021-03-01 20:26:17 +0100

    Added navbar toggle function and updated import of font awesome

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,38 @@
 import { NavLink } from "react-router-dom";
 import React, { useState } from "react"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 import styles from "./css/Navbar.module.css";
 
 function Navbar () {
-	//use boolean to create toggle effect on hamburger menu
-	const [navbarOpen, setNavbarOpen] = useState(false)
+	const [showMenu, setShowMenu] = useState(false)
 
-	const handleToggle = () => {
-		setNavbarOpen(prev => !prev)
-	}
-
-	const closeMenu = () => {
-		setNavbarOpen(false)
+	let dropdownMenu
+	if (showMenu) {
+		dropdownMenu = 
+			<ul>
+			<NavLink to="/">Home</NavLink>
+			<NavLink to="/">About us</NavLink>
+			</ul>
 	}
 
 	return (
-		<nav className="navBar">
-				<div className={styles.navbarWrapper}>
-				<button onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</button>
-					<ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
-						<NavLink 
-						to="/"
-						activeClassName="active-link"
-  					onClick={() => closeMenu()}
-  					exact>
-						Home
-						</NavLink>
-						<NavLink 
-						to="/"
-						activeClassName="active-link"
-  					onClick={() => closeMenu()}
-  					exact>
-						About
-						</NavLink>
-					</ul>
+		<nav className={styles.navbarWrapper}>
+			<div className={styles.navbar}>
+				<FontAwesomeIcon 
+					icon={faBars}
+					onClick={() => setShowMenu(!showMenu)}
+				/>
+
+				<div className={styles.centerLogo}>
+					<img src="https://play-lh.googleusercontent.com/vVBVzNF6g2ri-I0t8YSAdSkQY8_Vjra3HFBkkWkhgVo8IjmxOOeLgRAZWn8_7PrnYcs"></img>
 				</div>
+					
+				<span className={styles.cartAlignment}>cart</span>
+			</div>
+			
+			{ dropdownMenu }
 		</nav>
 	)
 }
