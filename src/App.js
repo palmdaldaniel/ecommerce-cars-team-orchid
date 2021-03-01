bfada33c651293cdc8c8f09bc385b0aecc90fd75
Saga <sagaswahn@hotmail.com>
2021-03-01 15:08:42 +0100

    Created routing in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <BrowserRouter>
+      <Route exact path="/"/>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
