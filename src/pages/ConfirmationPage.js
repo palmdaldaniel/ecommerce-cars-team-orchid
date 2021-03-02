e9d0bb79faeebad7519919db46c70427a150bcc9
Saga <sagaswahn@hotmail.com>
2021-03-02 15:30:37 +0100

    Changed code indenting

--- src/pages/ConfirmationPage.js
+++ src/pages/ConfirmationPage.js
@@ -4,12 +4,11 @@ const ConfirmationPage = () => {
   return (
     <div class="center-align">
       <div className={styles.title}>
-      <h4>Great! Enjoy your new-to-you car</h4>
+        <h4>Great! Enjoy your new-to-you car</h4>
       </div>
 
       <input type="checkbox" checked="checked" />
       <span>Purchase confirmed</span>
-
     </div>
   );
 }
