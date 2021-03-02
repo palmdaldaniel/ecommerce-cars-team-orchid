aa64d64b3df4f6d3ed1620974b6f334438c833a0
Axel Björnfot <axelbjornfot@gmail.com>
2021-03-02 20:33:24 +0100

    ändra namn på div'arna

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,27 +3,36 @@ import styles from "./css/About.module.css";
 const About = () => {
     return (
         <div className={styles.about}>
-            <div className={styles.imgContainer}>
+            <div className={styles.containerBox}>
+                <div className={styles.flexBox}>
                 <img src="https://images.pexels.com/photos/3876399/pexels-photo-3876399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=400" alt="A road in a valley" />
-            </div>
-            <div className={styles.contentContainer}>
+            
+            <div className={styles.content}>
                 <h1>About us</h1>
                 <p> This medium needs to be more dynamic. Five-year strategic plan do i have consent to record this meeting, we need to build it so that it scales imagineer. No scraps hit the floor circle back move the needle, so product launch back-end of third quarter and timeframe. Prethink golden goose back of the net, or tbrand terrorists, but on this journey yet this medium needs to be more dynamic. Close the loop drink from the firehose, but social currency i don't want to drain the whole swamp, i just want to shoot some alligators big picture, put it on the parking lot. You better eat a reality sandwich before you walk back in that boardroom quantity nor dog and pony show. Put your feelers out we’re starting to formalize flexible opinions around our foundations hammer out, but ping me. Land it in region.</p>
                 <p><i class="fas fa-phone-alt"></i><strong>Hit us up</strong><br/>
                 Phone: 0549-323 232<br/>
                 Mail: Carsagainshumanity@newcar.com</p>
+                </div>
             </div>
-            <div className={styles.imgContainer}>
+            </div>
+            <div className={styles.container}>
+                <div className={styles.flexBoxn}>
                 <img src="https://images.pexels.com/photos/1644794/pexels-photo-1644794.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=400" alt="A road in front of mountains" />
-            </div>
-            <div className={styles.contentContainer}>
+            
+            <div className={styles.content}>
                 <p> Wiggle room diversify kpis. Fire up your browser create spaces to  explore what’s next but I have zero cycles for this make it a priority but market-facing, for i have a hard stop in an hour and half turd polishing. Feed the algorithm. Staff engagement ping me let's unpack that later. My grasp on reality right now is tenuous productize. I have a hard stop in an hour and half let's put a pin in that so tribal knowledge. Create spaces to explore what’s next what about scaling components to a global audience? wheelhouse, or open door policy, but going forward, nor groom the backlog. We need to build it so that it scales groom the backlog. Low-hanging fruit shotgun approach, yet imagineer.</p>
+                 </div>
+             </div>
             </div>
-            <div className={styles.imgContainer}>
+            <div className={styles.container}>
+                <div className={styles.flexBox}>
                 <img src="https://images.pexels.com/photos/4099305/pexels-photo-4099305.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=400" alt="Two people sitting on a car in the forrest" />
-            </div>
-            <div className={styles.contentContainer}>
+            
+                <div className={styles.content}>
                 <p>Increase the pipelines talk to the slides. Helicopter view we need to dialog around your choice of work attire, yet bake it in. Q1 knowledge is power that's not on the roadmap but get six alpha pups in here for a focus group shoot me an email low engagement. We have to leverage up the messaging cross sabers, nor going forward, for synergestic actionables but run it up the flag pole the horse is out of the barn. Imagineer punter cross pollination across our domains we don't need to boil the ocean here nor you better eat a reality sandwich before you walk back in that boardroom vertical integration. Organic growth. Goalposts. Run it up the flag pole value-added, but hit the ground running gain traction not the long pole in my tent, but moving the goalposts.</p>
+                </div>
+             </div>
             </div>
         </div>
     );
