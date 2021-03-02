bb1d2dd9498e6e251806b35e60f84deb0838dfea
Saga <sagaswahn@hotmail.com>
2021-03-02 12:22:25 +0100

    Moved Cart Context to src and added Cart Context Provider in app.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,26 @@ import Confirmed from './pages/ConfirmationPage'
 import Checkout from './pages/Checkout.js'
 import Product from './pages/Product.js'
 import About from './pages/About';
+import CartContextProvider from './contexts/CartContext'
 
 function App() {
   return <div className="App">
     <BrowserRouter>
-      <Route exact path="/">
-        <Home/>
-      </Route>
-      <Route exact path="/Confirmed">
-        <Confirmed />
-      </Route>
-      <Route exact path="/checkout">
-        <Checkout/>
-      </Route>
-      <Route exact path="/product/:id" component={Product} />
-      <Route exact path="/about">
-        <About/>
-      </Route>
+      <CartContextProvider>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/Confirmed">
+          <Confirmed />
+        </Route>
+        <Route exact path="/checkout">
+          <Checkout/>
+        </Route>
+        <Route exact path="/product/:id" component={Product} />
+        <Route exact path="/about">
+          <About/>
+        </Route>
+      </CartContextProvider>
     </BrowserRouter>
   </div>;
 }
