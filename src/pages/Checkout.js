4cd246c77fb96ee4c139ea463e7d9d4af576b6b7
Amanda <amandaelizaolsson@gmail.com>
2021-03-02 14:30:02 +0100

    Created form tag and input fields

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,7 +5,49 @@ function Checkout() {
         <div className={style.checkoutContainer}>
             <h1>Checkout</h1>
             {/* Shopping cart list */}
-            {/* Form for personal details */}
+            <form className={style.contactForm}>
+                <h2 className={style.formTitle}>Add contact details</h2>
+                <label>Name</label>
+                <input type="text"
+                placeholder="First name"
+                required
+                />
+                
+                <input type="text"
+                placeholder="Last name"
+                required
+                />
+
+                <label>Shipping Address</label>
+                <input type="text"
+                placeholder="Address"
+                required
+                />
+
+                <input type="text"
+                placeholder="Postal Code"
+                required
+                />
+
+                <input type="text"
+                placeholder="City"
+                required
+                />
+
+                <label>Contact details</label>
+                <input type="text"
+                placeholder="Email"
+                required
+                />
+
+                <label></label>
+                <input type="number"
+                placeholder="Phone Number"
+                required
+                />
+                
+
+            </form>
             {/* purchase button */}
         </div>
     )
