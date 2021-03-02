8a39048f3160428b3e6c1273bdd32e3dae0227ec
daniel <daniel.palmdal@gmail.com>
2021-03-02 15:49:20 +0100

    Created add to cart button

---
+++ src/components/AddToCartButton.js
@@ -0,0 +1,13 @@
+import style from "./css/AddToCartButton.module.css";
+
+const AddToCartButton = () => {
+  return (
+    <div className="AddToCartButton">
+      <button className={style}> Add To Cart </button> <br />
+    </div>
+  );
+};
+
+export default AddToCartButton;
+
+
