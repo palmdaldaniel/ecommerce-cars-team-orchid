33c48ea84bd3bc00322a26685160babccd01a620
Sara <saramattisson@live.se>
2021-03-02 22:06:48 +0100

    Button and link to homepage added

--- src/pages/ConfirmationPage.js
+++ src/pages/ConfirmationPage.js
@@ -1,6 +1,10 @@
 import styles from './css/Confirmed.module.css'
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const ConfirmationPage = () => {
+  const history = useHistory();
+
   return (
     <div class="center-align">
       <div className={styles.title}>
@@ -9,6 +13,10 @@ const ConfirmationPage = () => {
 
       <input type="checkbox" checked="checked" />
       <span>Purchase confirmed</span>
+
+      <div>
+        <button className={styles.button} onClick={() => history.push('/')}> Brows more cars </button>
+      </div>
     </div>
   );
 }
