72eb247fcfd947e082b2c9dfb5f66ee7e141e1c4
Marko <marko.zdravkovski@hotmail.com>
2021-03-02 16:01:41 +0100

    Created ProductItem component

---
+++ src/components/ProductItem.js
@@ -0,0 +1,19 @@
+const ProductItem = (props) => {
+  return (
+    <div>
+      <img src={props.data.image} alt="" />
+      <div>
+        <h1>{props.data.make}</h1>
+        <h4>{props.data.model}</h4>
+        <p>{props.data.year}</p>
+        <p>{props.data.miles} mil</p>
+        <p>{props.data.city}</p>
+        <p>{props.data.descShort}</p>
+        <p>Price: {props.data.price} kr</p>
+        <button>Add to cart</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
