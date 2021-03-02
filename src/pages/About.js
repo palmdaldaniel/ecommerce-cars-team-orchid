aa7c790f511dfe28eeb1ad5159c9efe8412f51fe
Axel Björnfot <axelbjornfot@gmail.com>
2021-03-02 09:16:45 +0100

    lorem ipsum och img container

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,13 +2,23 @@ const About = () => {
 
 
     return (
-      <div className="about">
-  <p>funkar</p>
-      
-      </div>
-  
+        <div className="about">
+            <div className="img-container"></div>
+            <p> This medium needs to be more dynamic. Five-year strategic plan do i have consent to record this meeting, we need to build it so that it scales imagineer, and even dead cats bounce. No scraps hit the floor circle back move the needle, so product launch back-end of third quarter and timeframe. Prethink golden goose back of the net, or tbrand terrorists, but on this journey yet this medium needs to be more dynamic. Close the loop drink from the firehose, but social currency i don't want to drain the whole swamp, i just want to shoot some alligators big picture, put it on the parking lot. You better eat a reality sandwich before you walk back in that boardroom quantity nor dog and pony show. Put your feelers out we’re starting to formalize flexible opinions around our foundations hammer out, but ping me. Land it in region.</p>
+
+
+
+            <div className="img-container"></div>
+            <p> Wiggle room diversify kpis. Fire up your browser create spaces to  explore what’s next but I have zero cycles for this make it a priority but market-facing, for i have a hard stop in an hour and half turd polishing. Feed the algorithm. Staff engagement ping me let's unpack that later. My grasp on reality right now is tenuous productize. I have a hard stop in an hour and half let's put a pin in that so tribal knowledge. Create spaces to explore what’s next what about scaling components to a global audience? wheelhouse, or open door policy, but going forward, nor groom the backlog. We need to build it so that it scales groom the backlog. Low-hanging fruit shotgun approach, yet imagineer.</p>
+
+
+            <div className="img-container"></div>
+            <p>Increase the pipelines talk to the slides. Helicopter view we need to dialog around your choice of work attire, yet bake it in. Q1 knowledge is power that's not on the roadmap but get six alpha pups in here for a focus group shoot me an email low engagement. We have to leverage up the messaging cross sabers, nor going forward, for synergestic actionables but run it up the flag pole the horse is out of the barn. Imagineer punter cross pollination across our domains we don't need to boil the ocean here nor you better eat a reality sandwich before you walk back in that boardroom vertical integration. Organic growth. Goalposts. Run it up the flag pole even dead cats bounce value-added, but hit the ground running gain traction not the long pole in my tent, but moving the goalposts.</p>
+
+
+        </div>
+
     );
-  }
-  
-  export default About;
-  
+}
+
+export default About;
