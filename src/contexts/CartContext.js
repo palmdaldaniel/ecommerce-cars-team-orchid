c3c9aff908113a9d3db42981bd38a9891236bc9e
Amanda <amandaelizaolsson@gmail.com>
2021-03-03 15:06:00 +0100

    Removed placeholders from cart array

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -4,18 +4,17 @@ export const CartContext = createContext();
 
 function CartContextProvider(props) {
 	const [cart, setCart] = useState([
-		{
-			car: "old volvo",
-			age: "old"
-		},
-		{
-			car: "another old volvo",
-			age: "even older"
-		}
+		
 	]);
 
+	//triggers when user clicks on purchase button on checkout page
 	const handlePurchase = () => {
 		console.log("a purchase has been made")
+		
+		//empties the cart. will be changed once more logic is added
+		setCart(cart.length = 0)
+		console.log(cart)
+
 	}
 
 	const values = { cart, handlePurchase };
