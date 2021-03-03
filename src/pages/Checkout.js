3be304d62442b7e99c37f271b3453eef1adaffd1
Amanda <amandaelizaolsson@gmail.com>
2021-03-03 10:02:58 +0100

    Made some changes to form input fields

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -4,71 +4,101 @@ function Checkout() {
     return(
         <div className={style.checkoutContainer}>
             <h1>Checkout</h1>
-            {/* Shopping cart list */}
+            {/* Shopping cart list component */}
             <form className={style.contactForm}>
                 <h2 className={style.formTitle}>Delivery info</h2>
-                <label>Name</label>
-                <input type="text"
+                <label htmlFor="name">Name</label>
+                <input 
+                className={style.inputField}
+                type="text"
+                name="name"
                 placeholder="First name"
                 required
                 />
                 
-                <input type="text"
+                <input 
+                type="text"
                 placeholder="Last name"
                 required
                 />
-
-                <label>Shipping Address</label>
-                <input type="text"
+                
+                <label htmlFor="address">Shipping Address</label>
+                <input 
+                type="text"
+                name="address"
                 placeholder="Address"
                 required
                 />
 
                 <input type="text"
+                name="address"
                 placeholder="Postal Code"
                 required
                 />
 
                 <input type="text"
+                name="address"
                 placeholder="City"
                 required
                 />
 
-                <label>Contact details</label>
-                <input type="text"
+                <label htmlFor="contact">Contact details</label>
+                <input 
+                type="text"
+                name="contact"
                 placeholder="Email"
                 required
                 />
 
-                <label></label>
-                <input type="number"
+                <input 
+                type="number"
+                name="contact"
                 placeholder="Phone Number"
                 required
                 />
 
-                <h2>Payment</h2>
-                <label>Name on card</label>
-                <input type="text" placeholder="Full name" />
+                {/* <h2>Delivery options</h2>
+                <input type="radio" name="delivery" value="pickup"/>
+                <label>Pick up at store</label>
+                <input type="radio" name="delivery" value="delivered"/>
+                <label>Get car delivered to you</label> */}
 
-                <label>Card Number</label>
-                <input type="number" placeholder="Card number" required/>
+                <h2>Payment</h2>
+                <label htmlFor="cardname">Name on card</label>
+                <input 
+                type="text" 
+                name="cardname"
+                placeholder="Full name" />
+
+                <label htmlFor="cardnumber">Card Number</label>
+                <input 
+                type="number" 
+                name="cardnumber"
+                placeholder="Card number" 
+                required/>
                 
-                <label>CVV</label>
-                <input type="text" 
+                <label htmlFor="CVV">CVV</label>
+                <input 
+                type="text" 
+                name="CVV"
                 maxLength="3"
                 placeholder="CVV" 
                 required/>
 
-                    <label>Expiration Date</label>
-                    <div className={style.monthYearWrapper}>
-                        <input type="number" 
-                        placeholder="Month"
-                        required/>
-                        <input type="number"
-                        placeholder="Year"
-                        required />
+                <label htmlFor="expdate">Expiration Date</label>
+                <div className={style.monthYearWrapper}>
+                    <input 
+                    type="number" 
+                    name="expdate"
+                    placeholder="Month"
+                    required/>
+                    <input 
+                    type="number"
+                    name="expdate"
+                    placeholder="Year"
+                    required />
                     </div>
-                <input type="checkbox" />
+                
 
             </form>
             {/* purchase button */}
