9fcd1b8c2f51c158b545e67cd39c09a9ac08df48
Amanda <amandaelizaolsson@gmail.com>
2021-03-03 10:18:37 +0100

    Made label wrap around input fields
    To prevent input fields to connect to the wrong label (the one beneath it rather than the one above it)

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -3,59 +3,64 @@ import style from './css/Checkout.module.css'
 function Checkout() {
     return(
         <div className={style.checkoutContainer}>
-            <h1>Checkout</h1>
+            <h1 className={style.checkoutTitle}>Checkout</h1>
             {/* Shopping cart list component */}
             <form className={style.contactForm}>
                 <h2 className={style.formTitle}>Delivery info</h2>
-                <label htmlFor="name">Name</label>
-                <input 
-                className={style.inputField}
-                type="text"
-                name="name"
-                placeholder="First name"
-                required
-                />
+
+                <label htmlFor="name"><span>Name</span>
+                    <input 
+                    className={style.inputField}
+                    type="text"
+                    name="name"
+                    placeholder="First name"
+                    required
+                    />
                 
-                <input 
-                type="text"
-                placeholder="Last name"
-                required
-                />
+                    <input 
+                    type="text"
+                    placeholder="Last name"
+                    required
+                    />
+                </label>
                 
-                <label htmlFor="address">Shipping Address</label>
-                <input 
-                type="text"
-                name="address"
-                placeholder="Address"
-                required
-                />
+                <label htmlFor="address"><span>Shipping Address</span>
+                    <input 
+                    type="text"
+                    name="address"
+                    placeholder="Address"
+                    required
+                    />
 
-                <input type="text"
-                name="address"
-                placeholder="Postal Code"
-                required
-                />
+                    <input type="text"
+                    name="address"
+                    placeholder="Postal Code"
+                    required
+                    />
+                
 
-                <input type="text"
-                name="address"
-                placeholder="City"
-                required
-                />
+                    <input type="text"
+                    name="address"
+                    placeholder="City"
+                    required
+                    />
+                </label>
 
-                <label htmlFor="contact">Contact details</label>
-                <input 
-                type="text"
-                name="contact"
-                placeholder="Email"
-                required
-                />
+                <label htmlFor="contact"><span>Contact details</span>
+                    <input 
+                    type="text"
+                    name="contact"
+                    placeholder="Email"
+                    required
+                    />
 
-                <input 
-                type="number"
-                name="contact"
-                placeholder="Phone Number"
-                required
-                />
+                    <input 
+                    type="number"
+                    name="contact"
+                    placeholder="Phone Number"
+                    required
+                    />
+                </label>
 
                 {/* <h2>Delivery options</h2>
                 <input type="radio" name="delivery" value="pickup"/>
@@ -63,41 +68,46 @@ function Checkout() {
                 <input type="radio" name="delivery" value="delivered"/>
                 <label>Get car delivered to you</label> */}
 
+
                 <h2>Payment</h2>
-                <label htmlFor="cardname">Name on card</label>
-                <input 
-                type="text" 
-                name="cardname"
-                placeholder="Full name" />
+                <label htmlFor="cardname"><span>Name on card</span>
+                    <input 
+                    type="text" 
+                    name="cardname"
+                    placeholder="Full name" />
+                </label>
 
-                <label htmlFor="cardnumber">Card Number</label>
-                <input 
-                type="number" 
-                name="cardnumber"
-                placeholder="Card number" 
-                required/>
-                
-                <label htmlFor="CVV">CVV</label>
-                <input 
-                type="text" 
-                name="CVV"
-                maxLength="3"
-                placeholder="CVV" 
-                required/>
-
-                <label htmlFor="expdate">Expiration Date</label>
-                <div className={style.monthYearWrapper}>
+                <label htmlFor="cardnumber"><span>Card Number</span>
                     <input 
                     type="number" 
-                    name="expdate"
-                    placeholder="Month"
+                    name="cardnumber"
+                    placeholder="Card number" 
                     required/>
+                </label>
+                
+                <label htmlFor="CVV"><span>CVV</span>
                     <input 
-                    type="number"
-                    name="expdate"
-                    placeholder="Year"
-                    required />
+                    type="text" 
+                    name="CVV"
+                    maxLength="3"
+                    placeholder="CVV" 
+                    required/>
+                </label>
+
+                <label htmlFor="expdate"><span>Expiration Date</span>
+                    <div className={style.monthYearWrapper}>
+                        <input 
+                        type="number" 
+                        name="expdate"
+                        placeholder="Month"
+                        required/>
+                        <input 
+                        type="number"
+                        name="expdate"
+                        placeholder="Year"
+                        required />
                     </div>
+                </label>
                 
 
             </form>
