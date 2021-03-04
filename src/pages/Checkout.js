89cb1eb612767233373bb4c9e91b7f0a2de475cb
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-03-04 15:10:52 +0100

    Create placeholder data for testing

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -6,9 +6,37 @@ import style from './css/Checkout.module.css'
 
 function Checkout() {
 
-    const { cart, cartValue, handlePurchase } = useContext(CartContext);
+    // const { cart, cartValue, handlePurchase } = useContext(CartContext);
+    const { cartValue, handlePurchase } = useContext(CartContext);
     const history = useHistory();
     const [valueStr, setPriceString] = useState("");
+
+    const [cart] = useState([
+        {
+            "make": "Chevrolet",
+            "model": "Camaro",
+            "year": 1973,
+            "vin": "1D4PT5GK0BW487259",
+            "city": "Santa Rosa",
+            "descShort": "in lectus pellentesque at nulla suspendisse potenti cras in purus eu",
+            "descLong": "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
+            "price": 554963,
+            "miles": 15432,
+            "image": "/assets/car-pictures/Chevrolet-Camaro-1973.jpg"
+        },
+        {
+            "make": "Pontiac",
+            "model": "Montana SV6",
+            "year": 2006,
+            "vin": "JN1CV6FE7DM360307",
+            "city": "Jāsim",
+            "descShort": "purus sit amet nulla quisque arcu libero rutrum ac lobortis vel dapibus at diam",
+            "descLong": "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.\n\nFusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.\n\nSed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.\n\nPellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
+            "price": 299379,
+            "miles": 12346,
+            "image": "/assets/car-pictures/Pontiac-Montana SV6-2006.jpg"
+        }
+    ]);
 
     useEffect(() => {
         if (typeof cartValue === "number") {
