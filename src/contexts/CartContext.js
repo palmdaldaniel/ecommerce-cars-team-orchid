00e0edacffb090e253fa48924614afae3610bdc0
Saga <sagaswahn@hotmail.com>
2021-03-04 11:24:11 +0100

    Removed all testing-code

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -4,42 +4,19 @@ export const CartContext = createContext();
 
 function CartContextProvider(props) {
 	const [cart, setCart] = useState([
-		{
-			car: "old Volvo", //placeholders for testing, can be deleted once testing is complete
-			age: "old"
-		},
-		{
-			car: "even older volvo",
-			age: "extremely old"
-		}
 	]);
 
-	//array for purchased cars to be rendered on confirmed page
+	//array for purchased cars to be rendered on confirmation page
 	const [ purchased, setPurchased] = useState([
 	]);
 
-	//used for testing, can be deleted later
-	const [ isCartChanged, setIsCartChanged] = useState(false)
-
-
 	//triggers when user clicks on purchase button on checkout page
 	const handlePurchase = () => {
-		console.log("a purchase has been made")
 		
-		//copies cart to purchased array and sets cart to an empty array 
-		setPurchased([...cart])
-		setCart([])
-
-		//used for testing, can be deleted later
-		setIsCartChanged(true)	
+	//copies cart to purchased array and sets cart to an empty array 
+	setPurchased([...cart])
+	setCart([])
 	}
-
-	//used for testing handlePurchase method, can be deleted later
-	useEffect(() => {
-		console.log(cart)
-		console.log(purchased)
-		setIsCartChanged(false)
-	}, [isCartChanged])
 
 	const values = { cart, purchased, handlePurchase };
 
