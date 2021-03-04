f6b2bac71d1aae68e3bad52c23687ada325e5d14
Marko <marko.zdravkovski@hotmail.com>
2021-03-04 13:27:59 +0100

    Formated price

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -2,12 +2,26 @@ import {
   container,
   productInfo,
   firstInfo,
-  price,
+  priceContainer,
   description,
 } from "./css/ProductItem.module.css";
 import AddToCartButton from "../components/AddToCartButton.js";
+import { useState, useEffect } from "react";
 
 const ProductItem = (props) => {
+  const [price, setPrice] = useState(null);
+
+  useEffect(() => {
+    if (props.data) {
+      setPrice(
+        props.data.price.toLocaleString(navigator.language, {
+          style: "currency",
+          currency: "SEK",
+        })
+      );
+    }
+  }, [props.data]);
+
   return (
     <div className={container}>
       <img src={props.data.image} alt={props.data.make} />
@@ -17,8 +31,8 @@ const ProductItem = (props) => {
           <h5>{props.data.model}</h5>
         </div>
         <p className={description}>{props.data.descShort}</p>
-        <div className={price}>
-          <span>Price: {props.data.price} kr</span>
+        <div className={priceContainer}>
+          <span>Price: {price}</span>
           <AddToCartButton />
         </div>
       </div>
