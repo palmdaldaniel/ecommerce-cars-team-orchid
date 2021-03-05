b7cfbb93e619a983caec9d313dc8ca32682093e6
Amanda <amandaelizaolsson@gmail.com>
2021-03-05 10:21:48 +0100

    Removed test code from cartContext and ConfirmationPage

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -3,30 +3,7 @@ import { createContext, useEffect, useState } from "react";
 export const CartContext = createContext();
 
 function CartContextProvider(props) {
-	const [cart, setCart] = useState([
-		{
-			make: "Ford",
-			model: "Econoline E150",
-			year: 2002,
-			vin: "JN8AS5MT8BW176945",
-			city: "Shanban",
-			descShort: "metus arcu adipiscing molestie hendrerit at vulputate vitae nisl aenean lectus",
-			descLong: "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.\n\nIn sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
-			price: 413322,
-			miles: 14266
-		  },
-		  {
-			make: "Mercury",
-			model: "Mountaineer",
-			year: 2009,
-			vin: "3GTU2YEJ4CG418654",
-			city: "Dili",
-			descShort: "purus sit amet nulla quisque arcu libero rutrum ac lobortis",
-			descLong: "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
-			price: 486455,
-			miles: 15226
-		  },
-	]);
+	const [cart, setCart] = useState([]);
 
 	//array for purchased cars to be rendered on confirmation page
 	const [purchased, setPurchased] = useState([]);
--- src/pages/ConfirmationPage.js
+++ src/pages/ConfirmationPage.js
@@ -1,21 +1,15 @@
 import styles from './css/Confirmed.module.css'
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { useContext } from 'react';
-import { CartContext } from '../contexts/CartContext';
 
 const ConfirmationPage = () => {
   const history = useHistory();
-
-  const { cart, purchasedValue, handlePurchase } = useContext(CartContext);
 
   return (
     <div className="center-align">
       <div className={styles.title}>
         <h4>Great! Enjoy your new-to-you car</h4>
       </div>
-
-      <p>{purchasedValue}</p>
 
       <input type="checkbox" checked="checked" />
       <span>Purchase confirmed</span>
