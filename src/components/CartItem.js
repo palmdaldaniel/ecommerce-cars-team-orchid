f29777e96311dba93469c5c3d160c83aaf0ba76d
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-03-05 10:27:09 +0100

    Tweak styling on CartItem and Checkout, for smaller screens

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,14 +3,15 @@ import styles from './css/CartItem.module.css'
 const CartItem = (props) => {
 
 	return (
-		<div className={styles.cartContainer}>
+		// <div className={styles.cartContainer}>
 			<div className={styles.itemContainer}>
 				<img className={styles.carImg} src={props.product.image}></img>
-				<p>{props.product.make}</p>
-				<p>{props.product.model}</p>
-				<p className={styles.price}>{props.product.price}</p>
+				<div className={styles.itemContent}>
+					<p className={styles.title}>{props.product.make} {props.product.model}</p>
+					<p className={styles.price}>{props.product.price}</p>
+				</div>
 			</div>
-		</div>
+		// </div>
 	);
 }
 
