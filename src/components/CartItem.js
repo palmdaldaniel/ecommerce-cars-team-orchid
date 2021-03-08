8d8247e9833ad7a5f3af3806febf34dc7efe1c97
Marko <marko.zdravkovski@hotmail.com>
2021-03-08 13:30:55 +0100

    Changed currency in CartItem
    Now all components have right currency

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,18 +1,33 @@
-import styles from './css/CartItem.module.css'
+import styles from "./css/CartItem.module.css";
+import { useState, useEffect } from "react";
 
 const CartItem = (props) => {
-console.log(props.product.make);
-	return (
-		// <div className={styles.cartContainer}>
-			<div className={styles.itemContainer}>
-				<img className={styles.carImg} src={props.product.image}></img>
-				<div className={styles.itemContent}>
-					<p className={styles.title}>{props.product.make} {props.product.model}</p>
-					<p className={styles.price}>{props.product.price}</p>
-				</div>
-			</div>
-		// </div>
-	);
-}
+  const [price, setPrice] = useState(null);
+
+  useEffect(() => {
+    if (props.product) {
+      setPrice(
+        props.product.price.toLocaleString(navigator.language, {
+          style: "currency",
+          currency: "SEK",
+        })
+      );
+    }
+  }, [props.product]);
+
+  return (
+    // <div className={styles.cartContainer}>
+    <div className={styles.itemContainer}>
+      <img className={styles.carImg} src={props.product.image}></img>
+      <div className={styles.itemContent}>
+        <p className={styles.title}>
+          {props.product.make} {props.product.model}
+        </p>
+        <p className={styles.price}>{price}</p>
+      </div>
+    </div>
+    // </div>
+  );
+};
 
 export default CartItem;
