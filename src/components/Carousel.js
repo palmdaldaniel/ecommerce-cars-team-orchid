93bb332d1e4327175c23b1bb21fcbbd1695f56a9
Saga <sagaswahn@hotmail.com>
2021-03-09 11:30:58 +0100

    Reworked filter method

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,46 +1,28 @@
 
 import React from 'react';
-import { useContext } from "react";
-
+import { useContext, useState, useEffect } from "react";
 import { ProductsContext } from "../contexts/ProductsContext";
 import ProductItem from "./ProductItem";
-
+import styles from "./css/Carousel.module.css"
 
 
 const ImageSlider = () => {
-		//const { products } = useContext(ProductsContext);
-	const products = [
-		{
-		"make": "Chevrolet",
-    "model": "Camaro",
-    "year": 1973,
-    "vin": "1D4PT5GK0BW487259",
-    "city": "Santa Rosa",
-    "descShort": "in lectus pellentesque at nulla suspendisse potenti cras in purus eu",
-    "descLong": "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
-    "price": 554963,
-    "miles": 15432
-	},
-	{
-		"make": "Pontiac",
-    "model": "Montana SV6",
-    "year": 2006,
-    "vin": "JN1CV6FE7DM360307",
-    "city": "Jāsim",
-    "descShort": "purus sit amet nulla quisque arcu libero rutrum ac lobortis vel dapibus at diam",
-    "descLong": "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.\n\nFusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.\n\nSed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.\n\nPellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
-    "price": 299379,
-    "miles": 12346
-	}
-]
+	const { products } = useContext(ProductsContext);
+	const [filtered, setFiltered] = useState([]);
 
-	const filtered = products.filter(product => product.make === 'Pontiac');
-	
+	useEffect(() => {
+		if (products) {
+			setFiltered(products.filter(product => product.price < 500000))
+		}
+	}, [products] ) 
+
 	return (
 		<div>
 			{products &&
         filtered.map((product, i) => (
-          <ProductItem key={i} id={i} data={product} />
+					<div className={styles.productItem} key={i}>
+						<ProductItem id={i} data={product} />
+					</div>
       ))}
 		</div>
 	)
