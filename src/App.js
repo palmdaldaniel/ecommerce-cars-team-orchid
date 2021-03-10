cfa51b1dd62ae7b1069d4b5d098b3567065b0820
Marko <marko.zdravkovski@hotmail.com>
2021-03-10 11:45:29 +0100

    Created ScrollToTop component and imported in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route } from "react-router-dom";
 
-import Home from './pages/Home.js'
-import Navbar from './components/Navbar.js'
-import Confirmed from './pages/ConfirmationPage'
-import Checkout from './pages/Checkout.js'
-import Product from './pages/Product.js'
-import About from './pages/About';
-import CartContextProvider from './contexts/CartContext'
+import Home from "./pages/Home.js";
+import Navbar from "./components/Navbar.js";
+import Confirmed from "./pages/ConfirmationPage";
+import Checkout from "./pages/Checkout.js";
+import Product from "./pages/Product.js";
+import About from "./pages/About";
+import CartContextProvider from "./contexts/CartContext";
+import ScrollToTop from "./components/ScrollToTop.js";
 
 import ProductsContextProvider from "./contexts/ProductsContext";
 
@@ -16,7 +17,8 @@ function App() {
       <ProductsContextProvider>
         <CartContextProvider>
           <BrowserRouter>
-            <Navbar/>
+            <ScrollToTop />
+            <Navbar />
             <Route exact path="/">
               <Home />
             </Route>
