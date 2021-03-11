d623ecee7d583c11e19a8edba1518f9424de1e91
Saga <sagaswahn@hotmail.com>
2021-03-11 14:43:30 +0100

    Adjusted herobanners height

--- src/components/Herobanner.js
+++ src/components/Herobanner.js
@@ -3,17 +3,15 @@ import styles from './css/Herobanner.module.css'
 const Herobanner = () => {
 	return (
 		<div className={styles.heroContainer}>
-		
-		<div className={styles.videoContainer}>
-		<video autoPlay loop muted>
-        <source src="./hero.mp4" type="video/mp4"/>
-        </video> 
-		</div>
-
+			<div className={styles.videoContainer}>
+				<video autoPlay loop muted>
+						<source src="./hero.mp4" type="video/mp4"/>
+				</video> 
+			</div>
 			<div className={styles.textContainer}>
 				<span>A way to access freedom</span>
 				<p>Find your car today</p>	
-				</div>
+			</div>
 		</div>
 	);
 }
