44d5880f7a5980c8704d35f8d431dfad0385af5f
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-03-15 16:05:01 +0100

    Create addUser function

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -7,8 +7,18 @@ function UserContextProvider(props) {
 	const [users, setUsers] = useState([]);
 	const [currentUser, setCurrentUser] = useState([]);
 
-	const values = {};
+	function addUser(username, password) {
+		const user = {
+			username,
+			password,
+			history: []
+		}
+
+		setUsers(...users, user);
+	}
 	
+	const values = { users, addUser };
+
 	return (
 		<UserContext.Provider value={values}>{props.children}</UserContext.Provider>
 	);
