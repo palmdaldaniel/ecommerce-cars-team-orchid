a3dbbe44fe993948ef1451f7f5e8dd199c938cd5
Amanda <amandaelizaolsson@gmail.com>
2021-03-15 16:18:52 +0100

    Added jsx to UserMenu

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -1,5 +1,40 @@
-const UserMenu = () => {
+import { useState } from "react";
 
+const UserMenu = () => {
+    // placeholder variabels, to be deleted once userContext is connected
+    const [userLoggedIn, setUserLoggedin] = useState(false)
+
+    function handleLogin(e) {
+        e.preventDefault();
+    }
+
+    return(
+        <div>
+            {!userLoggedIn &&
+            <div>
+                <form onSubmit={handleLogin}>
+                    <label>
+                        <input type="text" placeholder="Username" />
+                    </label>
+                    <label>
+                        <input type="text" placeholder="Password" />
+                    </label>
+                    <button>Log in</button>
+                </form>
+                <p>Not a user? Register here</p>
+            </div>
+            }
+
+            {/* register user */}
+
+            {userLoggedIn &&
+            <div>
+            {/* Logged in info*/}
+            <p>hej</p>
+            </div>
+            }
+        </div>
+    )
 }
 
 export default UserMenu;
