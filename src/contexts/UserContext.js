019ca665349d467a1984577e03487dc70be556bd
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-03-15 16:26:00 +0100

    Create verifyUser function

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,7 +5,7 @@ export const UserContext = createContext();
 function UserContextProvider(props) {
 
 	const [users, setUsers] = useState([]);
-	const [currentUser, setCurrentUser] = useState([]);
+	const [currentUser, setCurrentUser] = useState(undefined);
 
 	function addUser(username, password) {
 		const user = {
@@ -16,8 +16,22 @@ function UserContextProvider(props) {
 
 		setUsers(...users, user);
 	}
+
+	/*	Attempts to to log in user with provided credentials.
+	 *	Returns '0' if no user exists with the supplied username,
+	 *	'1' if username exists but password is wrong.
+	 *	Signs user in and returns '2' if credentials match.
+	 */
+	function verifyUser(username, password) {
+		const user = users.find(u => u.username === username);
+		if (!user) return 0; // No such user
+		if (user.password !== password) return 1; // Wrong password
+
+		setCurrentUser(user);
+		return 2; // User logged in
+	}
 	
-	const values = { users, addUser };
+	const values = { users, addUser, verifyUser };
 
 	return (
 		<UserContext.Provider value={values}>{props.children}</UserContext.Provider>
