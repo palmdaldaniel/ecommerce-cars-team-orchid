660426ccf02d98ef51dd4db53c33f9e5140773ee
Saga <sagaswahn@hotmail.com>
2021-03-17 12:02:51 +0100

    Combining all filter fields

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { useContext, useState } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { ProductsContext } from "../contexts/ProductsContext";
 import styles from './css/Filter.module.css'
 
 const Filter = () => {
 	const { filterCars } = useContext(ProductsContext);
 	const { products } = useContext(ProductsContext);
-	const [filtered, setFiltered] = useState();
+	const [filters, setFilters] = useState({
+		make: "",
+		model: "",
+		year: "",
+		price: "",
+	});
 
 	const handleChange = (e) => {
-		setFiltered(e.target.value)
-		//const filtered = e.target.value;
-		filterCars(filtered)
+		const filter = {...filters}
+		filter[e.target.id] = e.target.value;
+		setFilters(filter)
 	};	
+
+	useEffect(() => {
+		filterCars(filters)
+	}, [filters]) 
 				
   return (
 		<div className={styles.filterWrapper}>
@@ -20,8 +29,8 @@ const Filter = () => {
 		<div className={styles.filterContainer}>
 			<div className={styles.filter}>
 				
-				<select onChange={handleChange}>
-					<option>Brand</option>
+				<select onChange={handleChange} id="make">
+					<option value="">Brand</option>
 					{products &&
 						products
 							.filter((v, i, a) => a.findIndex(t => t.make === v.make) === i)
@@ -32,8 +41,8 @@ const Filter = () => {
 						})}
         </select>
 
-				<select onChange={handleChange}>
-				<option>Model</option>
+				<select onChange={handleChange} id="model">
+				<option value="">Model</option>
 				{products &&
 						products
 							.filter((v, i, a) => a.findIndex(t => t.model === v.model) === i)
@@ -44,8 +53,8 @@ const Filter = () => {
 						})}
 				</select>
 
-				<select onChange={handleChange}>
-				<option>Year</option>
+				<select onChange={handleChange} id="year">
+				<option value="">Year</option>
 				{products &&
 						products
 							.filter((v, i, a) => a.findIndex(t => t.year === v.year) === i)
@@ -56,17 +65,17 @@ const Filter = () => {
 						})}
 				</select>
 				
-				<select onChange={handleChange}>
-					<option>Price</option>
-					<option>0 - 150000</option>
-					<option>150000 - 300000</option>
-					<option>300000 - 400000</option>
-					<option>400000 - 500000</option>
-					<option>500000 - 700000</option>
+				<select onChange={handleChange} id="price">
+					<option value="">Price</option>
+					<option value="1">0 - 150000</option>
+					<option value="2"> 150000 - 300000</option>
+					<option value="3">300000 - 400000</option>	
+					<option value="4">400000 - 500000</option>
+					<option value="5">500000 - 700000</option>
 				</select>
 
 				<select onChange={handleChange}>
-					<option>Miles</option>
+					<option value="">Miles</option>
 					<option>0 - 1000</option>
 					<option>1000 - 5000</option>
 					<option>5000 - 10000</option>
