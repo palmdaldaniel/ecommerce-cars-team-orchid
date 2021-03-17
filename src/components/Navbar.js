1e444ff68d16256c76e88d1bfd44ee087bda19df
Marko <marko.zdravkovski@hotmail.com>
2021-03-17 13:18:40 +0100

    Created component OutsideClick and added to Navbar
    When dropdown menu is open and user is clicked outside menu, the menu is closing. When user is clicked on Logo all dropdown-menus are closing.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,122 +1,157 @@
-import React, { useContext, useState, useEffect } from "react";
-import { CartContext } from '../contexts/CartContext';
-import { useHistory } from 'react-router-dom';
+import React, { useContext, useState, useEffect, useRef } from "react";
+import { CartContext } from "../contexts/CartContext";
+import { useHistory } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faShoppingCart, faEllipsisH, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faShoppingCart,
+  faEllipsisH,
+  faAngleDoubleRight,
+} from "@fortawesome/free-solid-svg-icons";
 
-import CartItem from './CartItem.js';
+import OutsideClick from "./OutsideClick.js";
+import CartItem from "./CartItem.js";
 import styles from "./css/Navbar.module.css";
 
-function Navbar (props) {
-	const [displayNavMenu, setDisplayNavMenu] = useState(false)
-	const [displayCart, setDisplayCart] = useState(false)
-	const { cart, cartValue } = useContext(CartContext);
-	const history = useHistory();const [valueStr, setPriceString] = useState("");
+function Navbar(props) {
+  const [displayNavMenu, setDisplayNavMenu] = useState(false);
+  const [displayCart, setDisplayCart] = useState(false);
+  const { cart, cartValue } = useContext(CartContext);
+  const history = useHistory();
+  const [valueStr, setPriceString] = useState("");
 
-	//Function for total cost in nav-cart 
-    useEffect(() => {
-        if (typeof cartValue === "number") {
-            setPriceString((cartValue).toLocaleString(navigator.language, {style: 'currency', currency: 'SEK'}));
-        }
-	}, [cartValue]);
-	const maxCartItems = 4;
+  //Dropdown menu is closing if you click outside component
+  const outsideClick = useRef();
+  const handleClickOutside = () => {
+    setDisplayCart(false);
+    setDisplayNavMenu(false);
+  };
+  OutsideClick(outsideClick, handleClickOutside);
 
+  //If you click on the Logo, dropdown menu is closing and is redirecting to home page
+  const logoClick = () => {
+    history.push("/");
+    setDisplayCart(false);
+    setDisplayNavMenu(false);
+  };
 
-	//toggle hamburger menu, could be refactored into one single function with the other toggle function?
-	function toggleNavMenu() {
-		setDisplayNavMenu(!displayNavMenu)
-		setDisplayCart(false)
-	}
+  //Function for total cost in nav-cart
+  useEffect(() => {
+    if (typeof cartValue === "number") {
+      setPriceString(
+        cartValue.toLocaleString(navigator.language, {
+          style: "currency",
+          currency: "SEK",
+        })
+      );
+    }
+  }, [cartValue]);
+  const maxCartItems = 4;
 
-	//toggle cart menu, could be refactored into one single function with the other toggle function?
-	function toggleCartMenu() {
-		setDisplayCart(!displayCart)
-		setDisplayNavMenu(false)
-	}
+  //toggle hamburger menu, could be refactored into one single function with the other toggle function?
+  function toggleNavMenu() {
+    setDisplayNavMenu(!displayNavMenu);
+    setDisplayCart(false);
+  }
 
-	//closing the menu when user clicks on link in hamburger menu
-	function closeMenu() {
-		setDisplayNavMenu(false)
-	}
+  //toggle cart menu, could be refactored into one single function with the other toggle function?
+  function toggleCartMenu() {
+    setDisplayCart(!displayCart);
+    setDisplayNavMenu(false);
+  }
 
-	//closing the menu when user clicks on proceedbutton and redirects to checkout
-	function proceedToCheckOut(e) {
-		if (e.target.tagName === "BUTTON") {
-			setDisplayCart(false)
-			history.push('/checkout')
-		}
-	}
+  //closing the menu when user clicks on link in hamburger menu
+  function closeMenu() {
+    setDisplayNavMenu(false);
+  }
 
-	return (
-		<nav className={styles.navbarWrapper}>
-			<div className={styles.navbar}>
-				<FontAwesomeIcon 
-					className={styles.hamburgerMenu}
-					icon={faBars} 
-					onClick={toggleNavMenu}
-				/>
+  //closing the menu when user clicks on proceedbutton and redirects to checkout
+  function proceedToCheckOut(e) {
+    if (e.target.tagName === "BUTTON") {
+      setDisplayCart(false);
+      history.push("/checkout");
+    }
+  }
 
-				<div className={styles.desktopLinks}>
-					<NavLink to="/">Home</NavLink>
-					<NavLink to="/about">About us</NavLink>
-				</div>
+  return (
+    <nav className={styles.navbarWrapper} ref={outsideClick}>
+      <div className={styles.navbar}>
+        <FontAwesomeIcon
+          className={styles.hamburgerMenu}
+          icon={faBars}
+          onClick={toggleNavMenu}
+        />
 
-				<div className={styles.centerLogo}>
-					<img 
-						className={styles.navLogo} 
-						src="/OC-logo.jpg" 
-						onClick={() => history.push('/')}
-					/>
-				</div>
-				
-				<div className={styles.cartContainer} onClick={toggleCartMenu}>
-					<FontAwesomeIcon icon={faShoppingCart} className={styles.shoppingCart}/>
-					<span className={styles.cartNumber}>{cart.length}</span>
-				</div>
-			</div>
+        <div className={styles.desktopLinks}>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About us</NavLink>
+        </div>
 
-			{ displayNavMenu &&
-			<ul className={styles.navUL}>
-				<NavLink className={styles.a} onClick={() => closeMenu()} to="/">Home</NavLink>
-				<NavLink className={styles.a}  onClick={() => closeMenu()} to="/about">About us</NavLink>
-			</ul>}
+        <div className={styles.centerLogo}>
+          <img
+            className={styles.navLogo}
+            src="/OC-logo.jpg"
+            onClick={logoClick}
+          />
+        </div>
 
-			{ displayCart &&
-			<div className={styles.cartContent}>
-				<p className={styles.cartHeadline}>Shopping cart</p>
-				
-				{!cart.length && (
-					<p>Nothing here right now</p>
-				)}
+        <div className={styles.cartContainer} onClick={toggleCartMenu}>
+          <FontAwesomeIcon
+            icon={faShoppingCart}
+            className={styles.shoppingCart}
+          />
+          <span className={styles.cartNumber}>{cart.length}</span>
+        </div>
+      </div>
 
-				{cart.length > 0 && (
-					<div className={styles.cartContainer}>
-						{ cart.slice(0, maxCartItems).map((product, i) => 
-							<div key={i} className={styles.cartItem}>
-								<CartItem product={product}/>
-							</div>
-						)}
-						{ cart.length > maxCartItems && (
-							<div className={styles.moreItems}>
-								<FontAwesomeIcon icon={faEllipsisH} size="2x" />
-								<p>{cart.length - maxCartItems} more item(s) in checkout</p>
-							</div>
-						)}
-						<hr />
-						<p className={styles.cartTotal}>{valueStr}</p>
-					</div>
-				)}
+      {displayNavMenu && (
+        <ul className={styles.navUL}>
+          <NavLink className={styles.a} onClick={() => closeMenu()} to="/">
+            Home
+          </NavLink>
+          <NavLink className={styles.a} onClick={() => closeMenu()} to="/about">
+            About us
+          </NavLink>
+        </ul>
+      )}
 
-				{cart.length > 0 && (
-					<button
-						className={styles.proceedButton}
-						onClick={(e) => proceedToCheckOut(e)}
-					>To checkout <FontAwesomeIcon icon={faAngleDoubleRight} /></button>
-				)}
-			</div>}
-		</nav>
-	)
+      {displayCart && (
+        <div className={styles.cartContent}>
+          <p className={styles.cartHeadline}>Shopping cart</p>
+
+          {!cart.length && <p>Nothing here right now</p>}
+
+          {cart.length > 0 && (
+            <div className={styles.cartContainer}>
+              {cart.slice(0, maxCartItems).map((product, i) => (
+                <div key={i} className={styles.cartItem}>
+                  <CartItem product={product} />
+                </div>
+              ))}
+              {cart.length > maxCartItems && (
+                <div className={styles.moreItems}>
+                  <FontAwesomeIcon icon={faEllipsisH} size="2x" />
+                  <p>{cart.length - maxCartItems} more item(s) in checkout</p>
+                </div>
+              )}
+              <hr />
+              <p className={styles.cartTotal}>{valueStr}</p>
+            </div>
+          )}
+
+          {cart.length > 0 && (
+            <button
+              className={styles.proceedButton}
+              onClick={(e) => proceedToCheckOut(e)}
+            >
+              To checkout <FontAwesomeIcon icon={faAngleDoubleRight} />
+            </button>
+          )}
+        </div>
+      )}
+    </nav>
+  );
 }
 
 export default Navbar;
