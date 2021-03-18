fa69e2c99553dcfb0b2db882ec10f3061291d8c8
Saga <sagaswahn@hotmail.com>
2021-03-18 09:55:30 +0100

    Commit before refactoring code

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -23,67 +23,71 @@ const Filter = () => {
 	useEffect(() => {
 		filterCars(filters)
 	}, [filters]) 
-				
+	
+	
+
   return (
 		<div className={styles.filterWrapper}>
-		<p>Filter</p>
-		<div className={styles.filterContainer}>
-			<div className={styles.filter}>
-				
-				<select onChange={handleChange} id="make">
-					<option value="">Brand</option>
-					{products &&
-						products
-							.filter((v, i, a) => a.findIndex(t => t.make === v.make) === i)
-							.map((product, i) => {
-								return (
-								<option value={product.make} key={i}>{product.make}</option>
-							)
-						})}
-        </select>
+			<p>Filter</p>
+				<div className={styles.filterContainer}>
+					<div className={styles.filter}>
+						
+						<select onChange={handleChange} id="make">
+							<option value="">Brand</option>
+							{products &&
+								products
+									.filter((v, i, a) => a.findIndex(t => t.make === v.make) === i)
+									.map((product, i) => {
+										return (
+										<option value={product.make} key={i}>{product.make}</option>
+									)
+								})}
+						</select>
 
-				<select onChange={handleChange} id="model">
-				<option value="">Model</option>
-				{products &&
-						products
-							.filter((v, i, a) => a.findIndex(t => t.model === v.model) === i)
-							.map((product, i) => {
-								return (
-								<option value={product.model} key={i}>{product.model}</option>
-							)
-						})}
-				</select>
+						<select onChange={handleChange} id="model">
+						<option value="">Model</option>
+						{products &&
+								products
+									.filter((v, i, a) => a.findIndex(t => t.model === v.model) === i)
+									.map((product, i) => {
+										return (
+										<option value={product.model} key={i}>{product.model}</option>
+									)
+								})}
+						</select>
 
-				<select onChange={handleChange} id="year">
-				<option value="">Year</option>
-				{products &&
-						products
-							.filter((v, i, a) => a.findIndex(t => t.year === v.year) === i)
-							.map((product, i) => {
-								return (
-								<option value={product.year} key={i}>{product.year}</option>
-							)
-						})}
-				</select>
-				
-				<select onChange={handleChange} id="price">
-					<option value="">Price</option>
-					<option value="1">0 - 150000</option>
-					<option value="2"> 150000 - 300000</option>
-					<option value="3">300000 - 400000</option>	
-					<option value="4">400000 - 500000</option>
-					<option value="5">500000 - 700000</option>
-				</select>
+						<select onChange={handleChange} id="year">
+						<option value="">Year</option>
+						{products &&
+								products
+									.filter((v, i, a) => a.findIndex(t => t.year === v.year) === i)
+									.map((product, i) => {
+										return (
+										<option value={product.year} key={i}>{product.year}</option>
+									)
+								})}
+						</select>
+						
+						<select onChange={handleChange} id="price">
+							<option value="">Price</option>
+							<option value="1">0 - 150000</option>
+							<option value="2">150000 - 300000</option>
+							<option value="3">300000 - 400000</option>	
+							<option value="4">400000 - 500000</option>
+							<option value="5">500000 - 700000</option>
+						</select>
 
-				<div className={styles.rangeContainer}>
-					<p>Miles: 0</p>
-					<input type="range" min="0" max="80000" step="1" onChange={handleChange} id="miles">
-					</input>
-					<p>Miles: {filters.miles}</p>
-				</div>
-				
-			</div>	
-		</div>
+						<select onChange={handleChange} id="miles">
+							<option value="">Miles</option>
+							<option value="1">0 - 150000</option>
+							<option value="2">150000 - 300000</option>
+							<option value="3">300000 - 500000</option>	
+							<option value="4">500000 - 700000</option>
+							<option value="5">700000 - 900000</option>
+						</select>
+
+					</div>	
+			</div>
 		</div>
   );
 };
