613c7b02246c29155b4fd81dcc1915d3c540acf7
daniel <daniel.palmdal@gmail.com>
2021-03-18 12:00:45 +0100

    Changed route path to vin instead of index

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -1,12 +1,15 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import styles from './css/CarouselItem.module.css'
+import styles from "./css/CarouselItem.module.css";
 
 const CarouselItem = (props) => {
-	const [price, setPrice] = useState(null);
-	const history = useHistory();
+	// destructuring for better readability
+  const { vin } = props.data;
 
-	useEffect(() => {
+  const [price, setPrice] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
     if (props.data) {
       setPrice(
         props.data.price.toLocaleString(navigator.language, {
@@ -15,26 +18,24 @@ const CarouselItem = (props) => {
         })
       );
     }
-	}, [props.data]);
+  }, [props.data]);
 
   const handleClick = () => {
-    history.push(`/product/${props.id}`);
+    history.push(`/product/${vin}`);
   };
-	
-	return (
-		<div className={styles.slideContainer}>
-			<div className={styles.imageContainer} onClick={handleClick}>
-				<img src={props.data.image} className={styles.image}/>
-					<div className={styles.textContainer}>
-						<span className={styles.bold}>{props.data.make}</span>
-						<span>{props.data.model}</span>
-						<div className={styles.price}>
-							{price}
-						</div>
-					</div>
-			</div>
-		</div>
-	);
-}
+
+  return (
+    <div className={styles.slideContainer}>
+      <div className={styles.imageContainer} onClick={handleClick}>
+        <img src={props.data.image} className={styles.image} />
+        <div className={styles.textContainer}>
+          <span className={styles.bold}>{props.data.make}</span>
+          <span>{props.data.model}</span>
+          <div className={styles.price}>{price}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CarouselItem;
