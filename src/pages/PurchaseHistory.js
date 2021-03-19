059852155630ffdb3d538325762a85b508031dcf
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-03-19 14:39:07 +0100

    Add basic functionality to PurchaseHistory page

--- src/pages/PurchaseHistory.js
+++ src/pages/PurchaseHistory.js
@@ -1,32 +1,42 @@
 import { useContext } from "react";
 
 import { UserContext } from "../contexts/UserContext";
+import { CartContext } from "../contexts/CartContext";
 import CartItem from "../components/CartItem";
-import style from "css/PurchaseHistory.module.css";
+import style from "./css/PurchaseHistory.module.css";
 
 function PurchaseHistory() {
 
 	const { currentUser } = useContext(UserContext);
+	const { cart, cartValue } = useContext(CartContext);
 
 	return (
 		<div className={style.container}>
 			<h1>Purchase history</h1>
 
-			{currentUser.history.map((purchase, i) => (
+			{currentUser &&
+			currentUser.history.map((purchase, i) => (
 				<div className={style.purchase} key={i}>
 					<h2>{purchase.timestamp}</h2>
 
 					<div className={style.cartList}>
-						{cart.map((product, i) => (
+						{purchase.products.map((product, i) => (
 							<div key={i} className={style.productRow}>
 								<CartItem product={product} />
 							</div>
 						))}
 					</div>
 
-					<p className={style.totalCost}>{valueStr}</p>
+					<p className={style.totalCost}>
+						{purchase.products.reduce((acc, val) => acc + val.price, 0).toLocaleString(
+							navigator.language,
+							{ style: "currency", currency: "SEK",}
+						)}
+					</p>
 				</div>
 			))}
 		</div>
 	);
 }
+
+export default PurchaseHistory;
